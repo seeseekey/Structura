e6584d91f4d87a753d6a6f721fdf596d702f3a1f
seeseekey <seeseekey@gmail.com>
2013-02-25 10:17:25 +0100

    Grafikadapeter weiter implementiert.

--- Structura/Constants.cs
+++ Structura/Constants.cs
@@ -4,10 +4,16 @@ namespace Structura
 {
     public static class Constants
     {
-        public const Int64 OverlayZoneStart=9000000000000000000; 
+        public const Int64 OverlayZoneStart=9000000000000000000;
 
-        public const Int64 GraphicMemoryAdressStart=9000000000000000000;
-        public const Int64 GraphicMemoryAdressEnd=9099999999999999999;
+		public const Int64 GraphicMemoryAdressStart=9000000000000000000;
+		public const Int64 GraphicMemoryAdressEnd=9099999999999999999;
+
+		public const Int64 GraphicMemoryConfigurationAdressStart=9000000000000000000;
+		public const Int64 GraphicMemoryConfigurationAdressEnd=9000000000000004095;
+
+		public const Int64 GraphicMemoryDisplayAdressStart=9000000000000004096;
+		public const Int64 GraphicMemoryDisplayAdressEnd=9099999999999999999;
         
         public const Int64 KeyboardMemoryAdressStart=9100000000000000000;
         public const Int64 KeyboardMemoryAdressEnd=9100000000000008191;
--- Structura/Graphic.cs
+++ Structura/Graphic.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using CSCL.Graphic;
+using System.IO;
+using System.Drawing;
 
 namespace Structura
 {
@@ -10,9 +12,19 @@ namespace Structura
     {
         byte[] data;// { get; private set; }
 
+		Int64 width=500;
+		Int64 height=500;
+
         public Graphic()
         {
             data=new byte[8192]; //8 Kilobyte
+
+			MemoryStream stream=new MemoryStream(data);
+			BinaryWriter writer=new BinaryWriter(stream);
+			writer.Write(width); //Bildschirmbreite
+			writer.Write(height); //Bildschirmhöhe 
+			writer.Flush();
+			writer.Close();
         }
 
         public byte[] GetData(Int64 offset, Int64 count)
@@ -46,7 +58,20 @@ namespace Structura
         public void Display()
         {
             gtImage image=new gtImage(500, 500, gtImage.Format.RGB);
-            image.FillWithMandelbrot();
+
+			for(Int64 i=Constants.GraphicMemoryDisplayAdressStart; i<width*height; i++)
+			{
+				Int64 x=0;
+				Int64 y=0;
+
+				byte r=data[i+0];
+				byte g=data[i+1];
+				byte b=data[i+2];
+				byte a=data[i+3];
+
+				image.SetPixel((int)x, (int)y, Color.FromArgb(a,r,g,b));
+			}
+
             image.SaveToPNGGDI("display.png");
         }
     }
