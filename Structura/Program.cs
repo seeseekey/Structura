2d8b25175f7fae6e5af52735b948cb6d49d79b67
seeseekey <seeseekey@gmail.com>
2013-03-03 11:43:11 +0100

    Mit der Implementation von TraceExecution begonnen.

--- Structura/Program.cs
+++ Structura/Program.cs
@@ -10,145 +10,169 @@ using Structura.Assembler;
 
 namespace Structura
 {
-	class Program
-	{
-		static UInt64 cycles=0;
+    class Program
+    {
+        static UInt64 cycles=0;
 
-		static Hardware.Structura cpu;
-		static Graphic graphic;
-		static int cycleInterval=1000;
-		static bool running=true;
+        static Hardware.Structura cpu;
+        static Graphic graphic;
+        static int cycleInterval=1000;
+        static bool running=true;
+        static bool traceExecution=false;
 
-		static void PrintInternalStates(Hardware.Structura cpu)
-		{
-			Console.Clear();
+        static void PrintInternalStates(Hardware.Structura cpu)
+        {
+            Console.Clear();
 
-			Console.WriteLine("Structura System v13.02");
-			Console.WriteLine("");
-			Console.WriteLine("Cycles: {0}", cycles);
-			Console.WriteLine("");
-			Console.WriteLine("Register:");
-			Console.WriteLine("A: {0}, B: {1}, C: {2}, D: {3}, E: {4}, F: {5}, G: {6}, H: {7}", cpu.A, cpu.B, cpu.C, cpu.D, cpu.E, cpu.F, cpu.G, cpu.H);
-			Console.WriteLine("I: {0}, J: {1}, K: {2}, L: {3}, M: {4}, N: {5}, O: {6}, P: {7}", cpu.I, cpu.J, cpu.K, cpu.L, cpu.M, cpu.N, cpu.O, cpu.P);
-			Console.WriteLine("Q: {0}, R: {1}, S: {2}, T: {3}, U: {4}, V: {5}, W: {6}, X: {7}", cpu.Q, cpu.R, cpu.S, cpu.T, cpu.U, cpu.V, cpu.W, cpu.X);
-			Console.WriteLine("Y: {0}, Z: {1}", cpu.Y, cpu.Z);
-			Console.WriteLine("");
-			Console.WriteLine("Special registers:");
-			Console.WriteLine("IC: {0}", cpu.IC);
-			Console.WriteLine("");
-			Console.WriteLine("Flags:");
-			Console.WriteLine("Zero: {0}, Positive: {1}, Negative: {2}, Overflow: {3}", cpu.Zero, cpu.Positive, cpu.Negative, cpu.Overflow);
-		}
+            Console.WriteLine("Structura System v13.02");
+            Console.WriteLine("");
+            Console.WriteLine("Cycles: {0}", cycles);
+            Console.WriteLine("");
+            Console.WriteLine("Register:");
+            Console.WriteLine("A: {0}, B: {1}, C: {2}, D: {3}, E: {4}, F: {5}, G: {6}, H: {7}", cpu.A, cpu.B, cpu.C, cpu.D, cpu.E, cpu.F, cpu.G, cpu.H);
+            Console.WriteLine("I: {0}, J: {1}, K: {2}, L: {3}, M: {4}, N: {5}, O: {6}, P: {7}", cpu.I, cpu.J, cpu.K, cpu.L, cpu.M, cpu.N, cpu.O, cpu.P);
+            Console.WriteLine("Q: {0}, R: {1}, S: {2}, T: {3}, U: {4}, V: {5}, W: {6}, X: {7}", cpu.Q, cpu.R, cpu.S, cpu.T, cpu.U, cpu.V, cpu.W, cpu.X);
+            Console.WriteLine("Y: {0}, Z: {1}", cpu.Y, cpu.Z);
+            Console.WriteLine("");
+            Console.WriteLine("Special registers:");
+            Console.WriteLine("IC: {0}", cpu.IC);
+            Console.WriteLine("");
+            Console.WriteLine("Flags:");
+            Console.WriteLine("Zero: {0}, Positive: {1}, Negative: {2}, Overflow: {3}", cpu.Zero, cpu.Positive, cpu.Negative, cpu.Overflow);
+        }
 
-		static void ShowHelp()
-		{
-			Console.Clear();
+        static void ShowHelp()
+        {
+            Console.Clear();
 
-			Console.WriteLine("Structura System Help");
-			Console.WriteLine("");
-			Console.WriteLine("Structura.exe program.asm");
-			Console.WriteLine("Structura.exe program.asm -cycleInterval:1000");
-			Console.WriteLine("Structura.exe program.asm -disassemble");
-			Console.WriteLine("");
-			Console.WriteLine("Parameter:");
-			Console.WriteLine("  -file:<filename>");
-			Console.WriteLine("  -cycleInterval:<timeInMilliSeconds");
-			Console.WriteLine("  -disassemble <-withIC>");
-		}
+            Console.WriteLine("Structura System Help");
+            Console.WriteLine("");
+            Console.WriteLine("Structura.exe program.asm");
+            Console.WriteLine("Structura.exe program.asm -cycleInterval:1000");
+            Console.WriteLine("Structura.exe program.asm -disassemble");
+            Console.WriteLine("");
+            Console.WriteLine("Parameter:");
+            Console.WriteLine("  -file:<filename>");
+            Console.WriteLine("  -cycleInterval:<timeInMilliSeconds");
+            Console.WriteLine("  -disassemble <-withIC>");
+            Console.WriteLine("  -traceExecution");
+        }
 
-		static void Main(string[] args)
-		{
-			Parameters arguments=Parameters.InterpretCommandLine(args);
+        static void Main(string[] args)
+        {
+            Parameters arguments=Parameters.InterpretCommandLine(args);
 
-			if(!arguments.Contains("file000")||arguments.Contains("h")||arguments.Contains("help")||arguments.Contains("?"))
-			{
-				ShowHelp();
-			}
+            if(!arguments.Contains("file000")||arguments.Contains("h")||arguments.Contains("help")||arguments.Contains("?"))
+            {
+                ShowHelp();
+            }
 
-			string filename=arguments.GetString("file000");
+            string filename=arguments.GetString("file000");
 
-			if(!File.Exists(filename))
-			{
-				Console.WriteLine("File don't exists.");
-				return;
-			}
+            if(!File.Exists(filename))
+            {
+                Console.WriteLine("File don't exists.");
+                return;
+            }
 
-			if(arguments.Contains("cycleInterval"))
-			{
-				cycleInterval=Convert.ToInt32(arguments.GetInt32("cycleInterval"));
-			}
+            if(arguments.Contains("cycleInterval"))
+            {
+                cycleInterval=Convert.ToInt32(arguments.GetInt32("cycleInterval"));
+            }
 
-			Console.CancelKeyPress+=new ConsoleCancelEventHandler(Console_CancelKeyPress);
+            Console.CancelKeyPress+=new ConsoleCancelEventHandler(Console_CancelKeyPress);
 
-			//Assemblieren
-			string[] asmCode=File.ReadAllLines(filename);
-			Int64[] machineCode=Assembler.Assembler.Assemble(asmCode);
-			byte[] machineCodeAsByteArray=new byte[machineCode.Length*8];
+            //Assemblieren
+            string[] asmCode=File.ReadAllLines(filename);
+            Int64[] machineCode=Assembler.Assembler.Assemble(asmCode);
+            byte[] machineCodeAsByteArray=new byte[machineCode.Length*8];
 
-			//Kopiere Int64 Array in ByteArray
-			for(int i=0; i<machineCode.Length; i++)
-			{
-				byte[] i64=BitConverter.GetBytes(machineCode[i]);
-				Array.Copy(i64, 0, machineCodeAsByteArray, i*8, 8);
-			}
+            //Kopiere Int64 Array in ByteArray
+            for(int i=0;i<machineCode.Length;i++)
+            {
+                byte[] i64=BitConverter.GetBytes(machineCode[i]);
+                Array.Copy(i64, 0, machineCodeAsByteArray, i*8, 8);
+            }
 
-			//Prüfe auf Disassembler
-			if(arguments.Contains("disassemble"))
-			{
-				bool withIC=arguments.GetBool("withIC", false);
-				List<string> disassembly=Disassembler.Disassemble(machineCodeAsByteArray, withIC);
+            //Prüfe auf Disassembler
+            if(arguments.Contains("disassemble"))
+            {
+                bool withIC=arguments.GetBool("withIC", false);
+                List<string> disassembly=Disassembler.Disassemble(machineCodeAsByteArray, withIC);
 
-				foreach(string line in disassembly)
-				{
-					Console.WriteLine(line);
-				}
+                foreach(string line in disassembly)
+                {
+                    Console.WriteLine(line);
+                }
 
-				return;
-			}
+                return;
+            }
 
-			//System initialisieren
-			graphic=new Graphic();
-			Keyboard keyboard=new Keyboard();
+            //System initialisieren
+            graphic=new Graphic();
+            Keyboard keyboard=new Keyboard();
 
-			Memory memory=new Memory();
-			memory.AddOverlayDevice(graphic);
-			memory.AddOverlayDevice(keyboard);
-			memory.WriteData(0, machineCodeAsByteArray);
+            Memory memory=new Memory();
+            memory.AddOverlayDevice(graphic);
+            memory.AddOverlayDevice(keyboard);
+            memory.WriteData(0, machineCodeAsByteArray);
 
-			cpu=new Hardware.Structura(memory);
+            cpu=new Hardware.Structura(memory);
 
-			//execute system
-			Thread thread=new Thread(new ThreadStart(ExecuteSystem));
-			thread.Start();
+            //Trace execution
+            traceExecution=arguments.Contains("traceExecution");
 
-			//Keyboard aktivieren
-			while(running)
-			{
-				ConsoleKeyInfo keyInfo=Console.ReadKey(true);
+            //execute system
+            Thread thread=new Thread(new ThreadStart(ExecuteSystem));
+            thread.Start();
 
-				byte modifier=(byte)keyInfo.Modifiers;
-				byte[] sign=Encoding.UTF32.GetBytes(keyInfo.KeyChar.ToString());
+            //Keyboard aktivieren
+            while(running)
+            {
+                ConsoleKeyInfo keyInfo=Console.ReadKey(true);
 
-				keyboard.AddKeyEvent(modifier, sign);
-			}
-		}
+                byte modifier=(byte)keyInfo.Modifiers;
+                byte[] sign=Encoding.UTF32.GetBytes(keyInfo.KeyChar.ToString());
 
-		static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
-		{
-			running=false;
-			e.Cancel=true; // Event abbrechen
-		}
+                keyboard.AddKeyEvent(modifier, sign);
+            }
+        }
 
-		static void ExecuteSystem()
-		{
-			while(running)
-			{
-				PrintInternalStates(cpu);
-				cpu.Execute();
-				graphic.Display();
-				cycles++;
-				Thread.Sleep(cycleInterval);
-			}
-		}
-	}
+        static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            running=false;
+            e.Cancel=true; // Event abbrechen
+        }
+
+        static void ExecuteSystem()
+        {
+            StreamWriter writer;
+
+            if(traceExecution)
+            {
+                writer=new StreamWriter("trace.txt");
+            }
+
+            while(running)
+            {
+                PrintInternalStates(cpu);
+				
+                string trace=cpu.Execute();
+
+                if(traceExecution)
+                {
+                    writer.WriteLine(trace);
+                    writer.Flush();
+                }
+
+                graphic.Display();
+
+                cycles++;
+                Thread.Sleep(cycleInterval);
+            }
+
+            if(traceExecution)
+                writer.Close();
+        }
+    }
 }
